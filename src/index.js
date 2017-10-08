994fe6461114f081d2e9aa1ebf76dbe31bc4f094
albert <albert.cymbaluk@gmail.com>
2017-10-08 21:37:35 +0200

    new

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { render } from 'react-dom';
+import {Provider} from 'react-redux';
+import store from './store/store';
+import { getCountries} from './actions/actions-countries';
+import DevTools from './DevTools';
+import { Router, hashHistory } from 'react-router';
+import routes from './routes';
+
+
+render(
+	<div>
+    	<Provider store={store}>
+        	<div>
+        	<DevTools />
+        		<Router history={hashHistory} routes={routes}/>
+        	</div>	
+    	</Provider>
+    </div>,
+    document.getElementById('root')
+);
+
+
+store.dispatch(getCountries());
+console.log(store.getState());
